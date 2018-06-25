eb96dc6c43c52de06695c90b9b80d79877eb6061
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-25 12:54:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,7 +22,13 @@ class Dog
   end
 
   def save
-    self
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
   end
 
 end
